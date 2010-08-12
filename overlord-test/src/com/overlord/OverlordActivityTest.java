b9e4d3736fd2fa330910eb7c4cf550817aa977ec
Hugo Josefson <hugo@josefson.org>
2010-08-12 15:24:02 +0200

    Added some tests which exercises Robotium Solo.

--- overlord-test/src/com/overlord/OverlordActivityTest.java
+++ overlord-test/src/com/overlord/OverlordActivityTest.java
@@ -15,8 +15,24 @@ import com.jayway.android.robotium.solo.Solo;
  */
 public class OverlordActivityTest extends ActivityInstrumentationTestCase2<OverlordActivity> {
 
+
+  private Solo solo;
+
   public OverlordActivityTest() {
     super("com.overlord", OverlordActivity.class);
+  }
+
+
+  public void setUp() throws Exception {
+    solo = new Solo(getInstrumentation(), getActivity());
+  }
+
+  public void testSoloIsNotNull() {
+    assertTrue(solo != null);
+  }
+
+  public void testHelloWorldTextIsFound() {
+    assertTrue(solo.searchText("Hello World"));
   }
 
 
